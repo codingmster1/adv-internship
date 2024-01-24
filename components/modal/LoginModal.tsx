3ed786009a4156ca1237c4910106e7fa4c496ae4
Jeff <doylejeff98@gmail.com>
2024-01-24 14:53:12 -0500

    using tailwin to customize modal login classes

--- components/modal/LoginModal.tsx
+++ components/modal/LoginModal.tsx
@@ -15,18 +15,43 @@ export default function LoginModal() {
                 Login
             </button>
 
+
             <Modal
                 open={isOpen}
                 onClose={handleClose}
+                className="fixed top-0 left-0 h-full flex justify-center 
+                items-center flex-col w-full bg-[rgba(0,0,0,0.5)]"
             >
-                <div className="w-[400px] h-[400px] bg-black">
-                    <h2 className="text-white">Log in to Summarist</h2>
-                    <h3 className="text-white">Login as Guest</h3>
-                    <h3 className="text-white">or</h3>
-                    <h3 className="text-white">Login with Google</h3>
+
+                <div>
+                    <div className="pt-12 px-8 pb-6">
+                        <h2 className="text-center font-bold text-[#032b41] text-xl mb-6">Log in to Summarist</h2>
+                    </div>
+                    <h3
+                        className="relative w-full flex bg-[#3a579d] text-white 
+                      justify-center items-center min-w-[180px] h-10 rounded text-base transition 
+                      duration-200 hover:bg-[#25396b]"
+                    >Login as Guest</h3>
+                    <h3>or</h3>
+                    <h3>Login with Google</h3>
+                    <input
+                        type="text"
+                        className="h-10 border-2 text-[#394547] border-[#bac8ce] 
+                     focus:border-[#2bd97c] rounded px-4"
+                        placeholder="Email Address" />
+                    <input
+                        className="h-10 border-2 border-[#bac8ce] rounded 
+                    text-[#394547] focus:border-[#2bd97c] p-3"
+                        type="password"
+                        placeholder="Password" />
+                    <button>Login</button>
+                    <button>Forgot your password</button>
+                    <button>Don't have an account </button>
                 </div>
+
 
             </Modal>
         </>
+
     )
 }
