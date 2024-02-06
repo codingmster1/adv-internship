75f1bf36834af827d10505c88a7caa9f99fa0c20
Jeff <doylejeff98@gmail.com>
2024-02-05 23:20:04 -0500

    imported logo and return statement

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,7 +7,7 @@ export default function Sidebar() {
             className="bg-[#f7faf9] md:inline md:w-[200px] md:min-w-[200px] fixed 
     top-0 left-0 h-screen transition-all duration-300 max-[768px]:-translate-x-full"
         >
-            <h1>test</h1>
+
             <div className="flex items-center justify-center h-[60px] pt-4 max-w-[160px] mx-auto">
                 <Image src={logo} className="w-full h-10" alt="logo" />
             </div>
