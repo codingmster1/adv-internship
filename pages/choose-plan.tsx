99e1ad4736ddc9a46324e59038c509bc4243318b
Jeff <doylejeff98@gmail.com>
2024-02-07 14:18:12 -0500

    export default function plan empty

---
+++ pages/choose-plan.tsx
@@ -0,0 +1,9 @@
+import Footer from "@/components/Footer";
+import { RiPlantFill, RiShakeHandsFill } from "react-icons/ri";
+
+export default function Plan() {
+    return (
+        <>
+        </>
+    );
+}
